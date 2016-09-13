0875d926e2eca5e2e9488f8088142210dc1ee40a
helenna <helena.gmas@gmail.com>
2016-09-13 17:57:54 +0200

    Documentos de prueba
    Aprendiendo PHP Foment 2016

---
+++ ifelse.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    
+   $edad = 17;
+    if($edad >= 18){
+        echo "Puedes acceder a la pagina";
+        }
+        else if($edad >=17){
+            echo "no puedes acceder a la pagina";
+        
+        }
+
+    
+    
+    
+    ?>
+    
+    
+    
+</body>
+</html>
